43dfa9fd796b14914a5526b8782a41987f5d0a16
abhattacha-mirp <abhattacha-mirp@mit.edu>
2017-05-31 12:33:36 +0530

    Add files via upload

---
+++ product_using_loops.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N,p;
+	cout<<"Enter a number:"<<endl;
+	cin>>N;
+	int i=0;
+	while(i<=10){
+	p=N*i;
+	cout<<"multiples:"<<p<<endl;
+	i++;
+	}
+	return 0;
+}
+	
